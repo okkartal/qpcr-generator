91a08c57e693dd8813b652c16bd7b48c5c8356c3
okadirkartal <161307071@kocaeli.edu.tr>
2019-05-27 02:48:08 +0300

    first commit

--- Qpcr.Core/QpcrGenerator.cs
+++ Qpcr.Core/QpcrGenerator.cs
@@ -3,22 +3,27 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using env = System.Environment;
 
 namespace Qpcr.Core
 {
     public class QpcrGenerator
     {
-        public List<CellItem> GenerateQpcr(QpcrModel model)
+        public Cell[,] GenerateQpcr(QpcrModel model, bool generateVisualizeTable = true)
         {
             ValidateQpcrModel(model);
+
+            int sumListOfIntegers = model.listOfIntegers.AsQueryable().Sum();
 
             //combine all reagents in single list
             var reAgentList = new List<string>();
             for (int i = 0; i < model.NamesOfReagents.Length; i++)
                 reAgentList.Add(model.NamesOfReagents[0, i]);
 
+
             var cellItems = new List<CellItem>();
 
+            Cell[,] cells = new Cell[model.Names.GetLength(0) + 1, sumListOfIntegers];
 
             //Get dimension count from Names array
             for (int dimensionIndex = 0; dimensionIndex < model.Names.Rank; dimensionIndex++)
@@ -26,7 +31,7 @@ namespace Qpcr.Core
                 //Get each element count from given dimension
                 for (int itemIndex = 0; itemIndex < model.Names.GetLength(dimensionIndex); itemIndex++)
                 {
-                    //this variable iterates 0 to sum(listOfInteger.values)
+                    //this variable iterates from  zero to sum(listOfInteger.values)
                     int listOfIntegerCounter = 0;
 
                     for (int listOfIntegerLen = 0; listOfIntegerLen < model.listOfIntegers.Length; listOfIntegerLen++)
@@ -35,18 +40,22 @@ namespace Qpcr.Core
                             listOfIntegerItem < model.listOfIntegers[listOfIntegerLen];
                             listOfIntegerItem++)
                         {
-                            //ig maximum number of plates not exceeded
+                            //if maximum number of plates not exceeded
                             if (model.MaximumNumberOfPlates == 0 || cellItems.Count < model.MaximumNumberOfPlates)
                             {
-                                CellItem item = new CellItem()
+                                cellItems.Add(new CellItem()
                                 {
                                     Name = model.Names[dimensionIndex, itemIndex],
                                     ReAgent = reAgentList[listOfIntegerLen],
                                     Row_Coord = itemIndex,
                                     Col_Coord = listOfIntegerCounter
-                                };
+                                });
 
-                                cellItems.Add(item);
+                                cells[itemIndex, listOfIntegerCounter] = new Cell()
+                                {
+                                    Name = model.Names[dimensionIndex, itemIndex],
+                                    ReAgent = reAgentList[listOfIntegerLen]
+                                };
 
                                 ++listOfIntegerCounter;
                             }
@@ -55,16 +64,20 @@ namespace Qpcr.Core
                 }
             }
 
-            var boardSize = GetBoardSize(model.PlateSize);
+            if (generateVisualizeTable)
+            {
+                var boardSize = GetBoardSize(model.PlateSize);
 
-            GenerateVisualTable(cellItems, boardSize.Item1, boardSize.Item2);
+                GenerateVisualizeTable(cellItems, boardSize.Item1, boardSize.Item2);
+            }
 
-            return cellItems;
+            return cells;
         }
+
 
         private (int, int) GetBoardSize(int plateSize)
         {
-            return (plateSize == Constants.PLATESIZE_96) ? (8, 18) : (16, 24);
+            return (plateSize == Constants.PLATESIZE_96) ? (8, 12) : (16, 24);
         }
 
         private void ValidateQpcrModel(QpcrModel model)
@@ -109,29 +122,32 @@ namespace Qpcr.Core
             return true;
         }
 
-        public void GenerateVisualTable(List<CellItem> cellItems, int rows, int columns)
+
+        private void GenerateVisualizeTable(IReadOnlyCollection<CellItem> cellItems, int rows, int columns)
         {
             StringBuilder sb = new StringBuilder();
-            sb.Append("<table  border='1'>");
-
+            sb.Append($"{env.NewLine}<table  border='1'>");
 
             for (int i = 0; i < rows; i++)
             {
-                sb.Append("<tr style='height:30px;'>");
+                sb.Append($"{env.NewLine}<tr style='height:30px;'>");
                 for (int j = 0; j < columns; j++)
                 {
                     var item = cellItems.Where(x => x.Row_Coord == i).FirstOrDefault(y => y.Col_Coord == j);
-                    sb.Append($"<td style='width:10px;background-color:{item?.ReAgent};'>");
+                    sb.Append($"{env.NewLine}<td style='width:10px;background-color:{item?.ReAgent};'>");
                     sb.Append(item?.Name);
                     sb.Append("</td>");
                 }
 
-                sb.Append("</tr>");
+                sb.Append($"{env.NewLine}</tr>");
             }
 
-            sb.Append("</table>");
+            sb.Append($"{env.NewLine}</table>");
 
-            File.WriteAllText("../../../report.html", $"<html><body>{sb.ToString()}</body</html>");
+            File.WriteAllText("../../../report.html", $"<html>{env.NewLine}<body>" +
+                                                      $"{env.NewLine}{sb.ToString()}" +
+                                                      $"{env.NewLine}</body>" +
+                                                      $"{env.NewLine}</html>");
         }
     }
 }
