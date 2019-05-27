44328fa8b58a1e755d91925ecf36cc10e80df18f
okadirkartal <161307071@kocaeli.edu.tr>
2019-05-27 04:58:42 +0300

    first commit

--- Qpcr.Core.Tests/QpcrCoreTests.cs
+++ Qpcr.Core.Tests/QpcrCoreTests.cs
@@ -16,27 +16,27 @@ namespace Qpcr.Core.Tests
                 PlateSize = 96
             };
         }
-        
+
         private static IEnumerable<QpcrModel> GetQpcrModelSecond()
         {
             yield return new QpcrModel()
             {
                 Names = new string[,] {{"Sam 1", "Sam 2", "Sam 3"}, {"Sam 1", "Sam 3", "Sam 4"}},
-                NamesOfReagents = new string[,] {{"Reag X", "Reag Y"},{"Reag Y", "Reag Z"}},
+                NamesOfReagents = new string[,] {{"Reag X", "Reag Y"}, {"Reag Y", "Reag Z"}},
                 listOfIntegers = new int[] {1, 3},
                 MaximumNumberOfPlates = 1,
                 PlateSize = 96
             };
         }
 
-         [TestCaseSource("GetQpcrModel")]
+        [TestCaseSource("GetQpcrModel")]
         public void CallMethod_WhenQpcrGenerated_ReturnsTrue(QpcrModel model)
         {
             var result = new QpcrGenerator().GenerateQpcr(model);
 
             Assert.IsNotNull(result);
         }
-        
+
         //[TestCaseSource("GetQpcrModelSecond")]
         public void CallMethod_WhenQpcrGeneratedSecond_ReturnsTrue(QpcrModel model)
         {
